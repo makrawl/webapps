d47149bf968315f9f39078f5cc65e6b44c350cd1
Rit <mail.ritsource@gmail.com>
2025-12-10 02:24:26 +0530

    fix: disable prop-types eslint rule

--- apps/makralabs-landing/src/app/layout.js
+++ apps/makralabs-landing/src/app/layout.js
@@ -1,6 +1,5 @@
 import { Cormorant_Garamond, Open_Sans } from "next/font/google";
 import "./globals.css";
-import PropTypes from "prop-types";
 
 const cormorant = Cormorant_Garamond({
   variable: "--font-cormorant",
@@ -13,12 +12,6 @@ const openSans = Open_Sans({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
-
-// const dmSans = DM_Sans({
-//   variable: "--font-dm-sans",
-//   subsets: ["latin"],
-//   weight: ["400", "500", "600", "700"],
-// });
 
 export const metadata = {
   title: "Makra Labs",
@@ -36,7 +29,3 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
-
-RootLayout.propTypes = {
-  children: PropTypes.node.isRequired,
-};
